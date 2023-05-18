2c996f5e5980ae67d268db06315c4559ac8d622a
Rachel <gggggrea@gmail.com>
2023-05-18 21:56:53 +0100

    Update sidebar style
    Update sidebar style

--- src/components/Sidebar/PlaceList.jsx
+++ src/components/Sidebar/PlaceList.jsx
@@ -2,24 +2,26 @@ import { useContext, useState } from "react";
 import { usePlaceData } from "../../hooks/use-place-data";
 import { PlaceContext, SearchContext } from "../../App";
 import { IoLocationSharp } from "react-icons/io5";
+import classNames from "classnames";
 import styles from "./PlaceList.module.css";
 
 export function PlaceList() {
   const { places } = usePlaceData({});
   const [searchData] = useContext(SearchContext);
-  const [, setClickedPlace] = useContext(PlaceContext);
+  const [clickedPlace, setClickedPlace] = useContext(PlaceContext);
 
   if (Object.keys(searchData).length === 0) return <div className={styles.blankList} />;
 
+  console.log(clickedPlace);
   return (
-    <div>
-      <div className={styles.placeSummary}>
-        {places?.features?.length} x {searchData?.searchPlace.toUpperCase()} FOUND
-      </div>
+    <div className={styles.placeList}>
       {places?.features?.map((place) => {
+        const isClicked = clickedPlace?.properties?.mapbox_id === place?.properties?.mapbox_id;
         return (
           <div
-            className={styles.placeContainer}
+            className={classNames(styles.placeContainer, {
+              [styles.highlighted]: isClicked,
+            })}
             onClick={() => {
               setClickedPlace(place);
             }}
@@ -27,7 +29,9 @@ export function PlaceList() {
           >
             <div className={styles.placeName}>{place.properties.name}</div>
             <div className={styles.placeAddress}>
-              <IoLocationSharp className={styles.placeLocator} /> {place.properties.address}
+              <IoLocationSharp className={styles.placeLocator} />
+              {place.properties.address}
+              {" | "}
               {place.properties.context.postcode?.name}
             </div>
           </div>
