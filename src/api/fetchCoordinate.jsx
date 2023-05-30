2c02a2a3bcc97f8a88b566b0f668dd5c4286b5cd
Rachel <gggggrea@gmail.com>
2023-05-30 20:58:52 +0100

    Add error handling when the postcode is invalid

--- src/api/fetchCoordinate.jsx
+++ src/api/fetchCoordinate.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export async function fetchCoordinate(postcode) {
-  const response = await axios.get(`https://api.postcodes.io/postcodes/${postcode}?`);
-
-  return response?.data;
+  try {
+    const response = await axios.get(`https://api.postcodes.io/postcodes/${postcode}?`);
+    return response.data;
+  } catch (error) {
+    alert(`Postcode [${postcode}] is invalid! Please try again.`);
+  }
 }
